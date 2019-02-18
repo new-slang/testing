39797d934bc7a2517847ea5f1b85cca60462acfa
Daniel Schindler <ds.daniel.schindler@gmail.com>
2019-02-18 15:45:36 +0100

    Adding
    tests

--- maxima.py
+++ maxima.py
@@ -37,3 +37,42 @@ def find_maxima(x):
             idx.append(i)
             check = False
     return idx
+
+def test_one():
+    x = [0, 1, 2, 1, 2, 1, 0]
+    print(find_maxima(x))
+    assert find_maxima(x) == [2, 4]
+
+    x = [-i**2 for i in range(-3, 4)]
+    assert find_maxima(x) == [3]
+
+    x = [np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)]
+    assert find_maxima(x) == [16, 78]
+
+def test_boundaries():
+    x = [4, 2, 1, 3, 1, 2]
+    assert find_maxima(x) == [0, 3, 5]
+
+    x = [4, 2, 1, 3, 1, 5]
+    assert find_maxima(x) == [0, 3, 5]
+
+    x = [4, 2, 1, 3, 1]
+    assert find_maxima(x) == [0, 3]
+
+def test_saddle():
+    x = [1, 2, 2, 1]
+    assert find_maxima(x) == [1, 2]
+    
+def test_four():
+    x = [1, 2, 2, 3, 1]
+    assert find_maxima(x) == [3]
+
+    x = [1, 3, 2, 2, 1]
+    assert find_maxima(x) == [1]
+
+    x = [3, 2, 2, 3]
+    assert find_maxima(x) == [0, 3]
+
+
+if __name__ == "__main__":
+    test_one()
